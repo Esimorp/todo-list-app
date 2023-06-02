fea1443fc2abe5c7eec0e24920022aee696d4da9
Jason Woo <dogsii@outlook.com>
2023-06-02 09:42:16 +0800

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { APP_RUNNING_PORT, logger } from './common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -9,6 +10,15 @@ async function bootstrap() {
   });
   const configService = app.get(ConfigService);
   const port = configService.getOrThrow<number>(APP_RUNNING_PORT);
+
+  const config = new DocumentBuilder()
+    .setTitle('Todolist App')
+    .setDescription('RESTFUL API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(port);
 }
